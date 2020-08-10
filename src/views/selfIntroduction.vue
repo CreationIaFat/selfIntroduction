35c4f485b3d7e3222baee288f26817469f57fbab
sotahMac <freestyle.baller.sk8er@gmail.com>
2020-08-10 22:30:29 +0900

    add kisiberohan

--- src/views/selfIntroduction.vue
+++ src/views/selfIntroduction.vue
@@ -1,6 +1,26 @@
 <template>
-    <div class="selfIntroduction row">
-        <div class="col-4">TopicArea</div>
-        <div class="col-8">ExplainArea</div>
+    <div class="row h-100">
+        <div class="col-4">
+            <div class="topicArea h-75">
+                <img alt="Rohan" src="../assets/kishiberohan.png" height="300px">
+            </div>
+        </div>
+        <div class="col-8">
+            <div class="explainArea h-75">ExplainArea</div>
+        </div>
+        <div class="col">
+            <div class="footerArea h-25">footerArea</div>
+        </div>
     </div>
 </template>
+<style lang="scss">
+.topicArea {
+    background-color: aqua;
+}
+.explainArea {
+    background-color:blanchedalmond;
+}
+.footerArea {
+    background-color: chartreuse;
+}
+</style>
