0b1b3caa31e7541862464af6deedd41d6c89acad
sotahMac <freestyle.baller.sk8er@gmail.com>
2020-08-16 18:49:33 +0900

    add selfIntroductionForIMGandVideo

--- src/components/organisms/SotahTopicArea.vue
+++ src/components/organisms/SotahTopicArea.vue
@@ -5,9 +5,30 @@
         </div>
         <div class="h-50">
             Todo:画像出す
-            {{ imgDataString }}
-            <div v-if="imgJudgment(imgDataString)">
-                <img alt="Rohan" src="../../assets/takoyaki.jpeg" height="300px">
+            {{ comment }}
+            <div v-if="TakoyakiJudgment(imgDataString)">
+                <img alt="Takoyaki" src="../../assets/takoyaki.jpeg" height="300px"  width="400px">
+            </div>
+            <div v-if="ElectricWorkJudgment(imgDataString)">
+                <img alt="ElectricWork"  src="../../assets/electricWork.jpg" height="300px"  width="400px">
+            </div>
+            <div v-if="FreestylefootballJudgment(imgDataString)">
+                <video alt="freestyle" loop autoplay muted src="../../assets/freestylefootball.mp4" height="300px"  width="400px" />
+            </div>
+            <div v-if="BilliardsJudgment(imgDataString)">
+                <img alt="Billiards"  src="../../assets/Billiards.jpeg" height="300px" width="400px">
+            </div>
+            <div v-if="ArduinoJudgment(imgDataString)">
+                <video alt="Arduino"  autoplay muted src="../../assets/Arduino.mp4" height="300px" width="400px" />
+            </div>
+            <div v-if="ProgrammingJudgment(imgDataString)">
+                <img alt="Programming"  src="../../assets/programming.png" height="300px"  width="400px">
+            </div>
+            <div v-if="SkateboardJudgment(imgDataString)">
+                <video alt="Skateboard"  autoplay muted src="../../assets/skateboard.mp4" height="300px" width="400px" />
+            </div>
+            <div v-if="BowlingJudgment(imgDataString)">
+                <img alt="Bowling" src="../../assets/Bowling.png" height="300px"  width="400px">
             </div>
         </div>
     </div>
@@ -24,9 +45,37 @@ import { Component, Vue, Prop } from "vue-property-decorator";
 export default class SotahTopicArea extends Vue{
     @Prop()
     public imgDataString?:string;
+    public comment?:string;
 
-    imgJudgment(payload:string){
+    TakoyakiJudgment(payload:string){
         return payload === "Takoyaki"? true: false;
+    }
+    ElectricWorkJudgment(payload:string){
+        return payload === "Electric Work"? true: false;
+        console.log("d");
+    }
+    ArduinoJudgment(payload:string){
+        return payload === "Arduino"? true: false;
+        console.log("d");
+    }
+    ProgrammingJudgment(payload:string){
+        return payload === "Programming"? true: false;
+        console.log("d");
+    }
+    FreestylefootballJudgment(payload:string){
+        return payload === "Freestyle football"? true: false;
+        console.log("d");
+    }
+    BilliardsJudgment(payload:string){
+        return payload === "Billiards"? true: false;
+        console.log("d");
+    }
+    SkateboardJudgment(payload:string){
+        return payload === "Skateboard"? true: false;
+        console.log("d");
+    }
+    BowlingJudgment(payload:string){
+        return payload === "Bowling"? true: false;
         console.log("d");
     }
 }
